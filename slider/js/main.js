0a35c977ec34b37e1864fbf9111af7205d28dd0f
Daria Goriana <goriana.d@gmail.com>
2021-03-27 23:55:26 +0200

    Smooth transition of slides + disabled/enabled buttons

--- slider/js/main.js
+++ slider/js/main.js
@@ -1,29 +1,51 @@
 let nextBtn = document.getElementById("next");
 let previousBtn = document.getElementById("previous");
 
-let slides = document.querySelectorAll("li"); //array of li items
+let slides = document.querySelectorAll("li");
 let slideIndex = 0;
 
-let removeActive = (index) => slides[index].classList.remove("active");
-let addActive = (index) => slides[index].classList.add("active");
+let sliderList = document.getElementById("sliderList");
+let shiftNum = 500;
+let listShift = 0;
 
-function showSlide(direction) {
+function getCurrentShift (currentShift, direction) {
+    return direction === 'next' ? currentShift - shiftNum : currentShift + shiftNum;
+}
+
+function showSlide (direction) {
     return function () {
         let isNextEnabled = slideIndex < slides.length - 1;
         let isPrevEnabled = slideIndex > 0;
+
+        let isFirstSlide = slideIndex === 0;
+        let isLastSlide = slideIndex === slides.length - 1;
+
         let currentIndex = slideIndex;
+
+        listShift = getCurrentShift(listShift, direction);
+
+        sliderList.style.transform = 'translateX(' + listShift + 'px)';
 
         if (isNextEnabled && direction === "next") {
             slideIndex++;
+            console.log('clicking next, index:', slideIndex);
         } else if (isPrevEnabled && direction === "previous") {
             slideIndex--;
+            console.log('clicking prev, index:', slideIndex);
         } else if (!isNextEnabled && direction === "next") {
             slideIndex = 0;
         } else if (!isPrevEnabled && direction === "previous") {
             slideIndex = slides.length - 1;
         }
-        removeActive(currentIndex);
-        addActive(slideIndex);
+
+        if (slideIndex === 0) {
+            previousBtn.setAttribute('disabled', '');
+        } else if (slideIndex > 0 && slideIndex !== slides.length - 1) {
+            previousBtn.removeAttribute('disabled');
+            nextBtn.removeAttribute('disabled');
+        } else if (slideIndex === slides.length - 1) {
+            nextBtn.setAttribute('disabled', '');
+        }
     };
 }
 
